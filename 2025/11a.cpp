d44c1010dc8878d497e03d4c32e310a7d7d30dde
ankitk26 <myselfankit51@gmail.com>
2025-12-12 06:07:27 +0530

    2025 11a simple

--- 2025/11a.cpp
+++ 2025/11a.cpp
@@ -23,8 +23,6 @@ int main() {
 	unordered_map<string, int> words;
 
 	int c = 0;
-	int start = -1, end = -1;
-
 	for (int i = 0; i < n; i++) {
 		string s; getline(cin, s);
 		int pos = s.find(":");
@@ -35,11 +33,6 @@ int main() {
 		if (words.find(from) == words.end()) {
 			words[from] = c;
 			src = c;
-			if (from == "you" && start == -1) {
-				start = c;
-			} else if (from == "out" && end == -1) {
-				end = c;
-			}
 			c++;
 		} else {
 			src = words[from];
@@ -57,12 +50,6 @@ int main() {
 				curr = s.substr(0, pos);
 			}
 			int tgt;
-
-			if (curr == "you" && start == -1) {
-				start = c;
-			} else if (curr == "out" && end == -1) {
-				end = c;
-			}
 
 			if (words.find(curr) == words.end()) {
 				words[curr] = c;
@@ -82,7 +69,9 @@ int main() {
 		}
 	}
 
+	int start = words["you"], end = words["out"];
 	int pathsCount = dfs(graph, start, end);
+
 	cout << pathsCount;
 
 	return 0;
